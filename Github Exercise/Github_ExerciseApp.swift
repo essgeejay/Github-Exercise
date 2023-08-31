cd15e494290b96c412eb5804fe2a7d4b7c691fae
essgeejay <essgeejay@email.com>
2023-08-31 10:11:38 +0100

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Shane Jeffrey on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
